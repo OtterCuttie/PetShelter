7dac318d267b700315bd8a2dcd66c38c37e5c8a5
CuttiePie <144522961+OtterCuttie@users.noreply.github.com>
2026-05-29 22:02:04 +0300

    Update FileManager.cs

--- MYPetShelter/Model/Data/FileManager.cs
+++ MYPetShelter/Model/Data/FileManager.cs
@@ -1,36 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model.Core;
 
 namespace Model.Data
 {
-
-    public abstract class FileManager
+    public abstract class FileManager : MyFileManager
     {
-        public abstract string FileExtension { get; }
-
-        public abstract void Save<T>(string path, T data);
-
-        public abstract T Load<T>(string path);
-
-        protected bool FileExists(string path)
+        protected FileManager(string name): base(name)
         {
-            string fullPath = path + FileExtension;
-            return File.Exists(fullPath);
         }
 
-        protected string GetFullPath(string path) => path + FileExtension;
-
-        protected void BackupIfExists(string path)
+        protected FileManager(string name,string folderPath,string fileName,string fileExtension): base(name, folderPath, fileName, fileExtension)
         {
-            string fullPath = GetFullPath(path);
-            if (File.Exists(fullPath))
-            {
-                string backupPath = fullPath + ".backup";
-                File.Copy(fullPath, backupPath, true);
-            }
         }
+
+        public abstract void Serialize(List<Shelter<Pet>> shelters);
+
+        public abstract List<Shelter<Pet>> Deserialize();
     }
 }
