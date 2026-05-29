72e5974bb1661e968bd33ef8e58dfcc782f74498
CuttiePie <144522961+OtterCuttie@users.noreply.github.com>
2026-05-29 22:03:18 +0300

    Update ShelterRepository.cs

--- MYPetShelter/Model/Data/ShelterRepository.cs
+++ MYPetShelter/Model/Data/ShelterRepository.cs
@@ -9,71 +9,96 @@ namespace Model.Data
 {
     public class ShelterRepository
     {
-        private readonly FileManager _serializer;
-        private readonly string _sheltersFilePath;
+        private readonly DataManager _dataManager;
+
         private List<Shelter<Pet>> _shelters;
 
-        public ShelterRepository(FileManager serializer, string dataDirectory = "Data")
+        public ShelterRepository(DataManager dataManager)
         {
-            _serializer = serializer;
-
-            if (!Directory.Exists(dataDirectory))
-                Directory.CreateDirectory(dataDirectory);
-
-            _sheltersFilePath = Path.Combine(dataDirectory, "shelters");
+            _dataManager = dataManager;
             LoadOrCreateShelters();
         }
 
         private void LoadOrCreateShelters()
         {
-            var loadedShelters = _serializer.Load<List<Shelter<Pet>>>(_sheltersFilePath);
-
-            if (loadedShelters == null || !loadedShelters.Any())
+            var loadedShelters =_dataManager.LoadShelters();
+            if (loadedShelters == null ||!loadedShelters.Any())
             {
-                _shelters = CreateTestShelters();
-                Save();
+                _shelters =CreateTestShelters();
+                _dataManager.SaveShelters(_shelters);
             }
             else
             {
-                _shelters = loadedShelters;
+                _shelters =loadedShelters;
             }
         }
 
-        private List<Shelter<Pet>> CreateTestShelters()
+        public List<Shelter<Pet>> GetAll()
+        {
+            return _shelters.ToList();
+        }
+
+        public Shelter<Pet> GetByName(string name)
+        {
+            return _shelters.FirstOrDefault(shelter => shelter.Name == name);
+        }
+
+        public void AddShelter(Shelter<Pet> shelter)
+        {
+            _shelters.Add(shelter);
+            _dataManager.SaveShelters(_shelters);
+        }
+
+        public void RemoveShelter(
+            Shelter<Pet> shelter)
+        {
+            _shelters.Remove(shelter);
+            _dataManager.SaveShelters(_shelters);
+        }
+
+        public void Save()
+        {
+            _dataManager.SaveShelters(_shelters);
+        }
+
+        private List<Shelter<Pet>>
+            CreateTestShelters()
         {
             var allPets = CreateTestPets();
-            var shelters = new List<Shelter<Pet>>();
+
+            var shelters =new List<Shelter<Pet>>();
 
             int petsPerShelter = 5;
             int currentIndex = 0;
 
-            // Приют 1
-            var shelter1 = new Shelter<Pet>("Солнечный приют", 20, true);
+            var shelter1 =
+                new Shelter<Pet>("Солнечный приют",20,true);
+
             foreach (var pet in allPets.Skip(currentIndex).Take(petsPerShelter))
             {
                 shelter1.AddPet(pet);
             }
+
             currentIndex += petsPerShelter;
             shelters.Add(shelter1);
+            var shelter2 =new Shelter<Pet>("Уютный дом",15,false);
 
-            // Приют 2
-            var shelter2 = new Shelter<Pet>("Уютный дом", 15, false);
             foreach (var pet in allPets.Skip(currentIndex).Take(petsPerShelter))
             {
                 shelter2.AddPet(pet);
             }
+
             currentIndex += petsPerShelter;
+
             shelters.Add(shelter2);
 
-            // Приют 3
-            var shelter3 = new Shelter<Pet>("Приятный приют", 30, true);
+            var shelter3 =new Shelter<Pet>("Приятный приют",30,true);
+
             foreach (var pet in allPets.Skip(currentIndex).Take(petsPerShelter))
             {
                 shelter3.AddPet(pet);
             }
-            currentIndex += petsPerShelter;
             shelters.Add(shelter3);
-
             return shelters;
         }
 
@@ -81,64 +106,28 @@ namespace Model.Data
         {
             var pets = new List<Pet>();
 
-            pets.Add(new Cat("Марк", 3, 5.5, "Сфинкс",2.2,false));
-            pets.Add(new Cat("Вил", 2, 4.2, "белоруссый",1.4, false));
-            pets.Add(new Cat("Снежок", 5, 6.0, "белый и пушистый", 4.3, true));
+            pets.Add(new Cat("Марк", 3, 5.5, "Сфинкс", 2.2, false));
+            pets.Add(new Cat("Вил", 2, 4.2, "Белоруссый", 1.4, false));
+            pets.Add(new Cat("Снежок", 5, 6.0, "Белый и пушистый", 4.3, true));
 
-            pets.Add(new Dog("Собяка-кобяка", 4, 15.5, "Очень милый и тихий","Дворняга", true));
-            pets.Add(new Dog("Рук", 2, 8.2, "очень любит играться с котами", "Коли", true));
-            pets.Add(new Dog("Рекс", 6, 28.0, "добрый и очень громкий", "Немецкая овчарка", true));
+            pets.Add(new Dog("Собяка-кобяка", 4, 15.5, "Очень милый и тихий", "Дворняга", true));
+            pets.Add(new Dog("Рук", 2, 8.2, "Любит играть с котами", "Коли", true));
+            pets.Add(new Dog("Рекс", 6, 28.0, "Добрый и громкий", "Немецкая овчарка", true));
 
-            pets.Add(new Rabbit("Бонни", 2, 2.5, "громко орет","синяя шерсть",15));
-            pets.Add(new Rabbit("Сильвер", 1, 1.8, "любит спать", "белая шерсть", 13));
-            pets.Add(new Rabbit("Ушастик", 3, 3.0,"ласковый","серая шерсть с черными пятнышками" , 17));
+            pets.Add(new Rabbit("Бонни", 2, 2.5, "Громко орет", "Синяя шерсть", 15));
+            pets.Add(new Rabbit("Сильвер", 1, 1.8, "Любит спать", "Белая шерсть", 13));
+            pets.Add(new Rabbit("Ушастик", 3, 3.0, "Ласковый", "Серая шерсть", 17));
 
-            pets.Add(new Fox("Алиса", 3, 6,"шрам на глазу",true,true));
-            pets.Add(new Fox("Фокси", 3, 6, "пират",true,false));
+            pets.Add(new Fox("Алиса", 3, 6, "Шрам на глазу", true, true));
+            pets.Add(new Fox("Фокси", 3, 6, "Пират", true, false));
 
-            pets.Add(new Parrot("Найджел", 3, 7, "очень противный, белый", 35,true));
-            pets.Add(new Parrot("Яго", 2, 6, "похож на попугая Джафара", 20, true));
+            pets.Add(new Parrot("Найджел", 3, 7, "Очень вредный", 35, true));
+            pets.Add(new Parrot("Яго", 2, 6, "Похож на попугая Джафара", 20, true));
 
-            pets.Add(new Raccoon("Камыш", 3, 6, "серый с белыми лапками и любит печеньки", 5, true));
-            pets.Add(new Raccoon("Разрушитель", 4, 6, "опасный, РАЗРУШИТЕЛЬ ВСЕГО", 10, false));
+            pets.Add(new Raccoon("Камыш", 3, 6, "Любит печеньки", 5, true));
+            pets.Add(new Raccoon("Разрушитель", 4, 6, "Разрушитель всего", 10, false));
+
             return pets;
-        }
-
-        public List<Shelter<Pet>> GetAll() => _shelters.ToList();
-
-        public Shelter<Pet> GetByName(string name)
-        {
-            return _shelters.FirstOrDefault(s => s.Name == name);
-        }
-
-        public void Add(Shelter<Pet> shelter)
-        {
-            if (!_shelters.Contains(shelter))
-            {
-                _shelters.Add(shelter);
-                Save();
-            }
-        }
-
-        public void Remove(Shelter<Pet> shelter)
-        {
-            _shelters.Remove(shelter);
-            Save();
-        }
-
-        public void Save()
-        {
-            _serializer.Save(_sheltersFilePath, _shelters);
-        }
-
-        public List<Pet> GetAllPets()
-        {
-            var allPets = new List<Pet>();
-            foreach (var shelter in _shelters)
-            {
-                allPets.AddRange(shelter.GetPets());
-            }
-            return allPets;
         }
     }
 }
