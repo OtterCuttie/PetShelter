4d96f1cacdf05f0f35fe08bde4458fb8c4645660
CuttiePie <144522961+OtterCuttie@users.noreply.github.com>
2026-05-29 22:00:41 +0300

    Update Shelter.cs

--- MYPetShelter/Model/Core/Shelter.cs
+++ MYPetShelter/Model/Core/Shelter.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Model
+namespace Model.Core
 {
     public partial class Shelter<T> : ICountable where T : Pet
     {
@@ -50,6 +50,64 @@ namespace Model
             return _pets.Where(filterDelegate)
                        .Cast<Pet>()
                        .ToList();
+        }
+    }
+
+    public partial class Shelter<T> where T : Pet
+    {
+        /// Перегрузка метода Filter для фильтрации по виду и клаустрофобии
+        public List<Pet> Filter(Type animalType, bool hasClaustrophobia)
+        {
+            // Создаем делегат для фильтрации по типу
+            Func<T, bool> typeFilter = (pet) => pet.GetType() == animalType;
+
+            // Создаем делегат для фильтрации по клаустрофобии
+            Func<T, bool> claustrophobiaFilter = (pet) => pet.HasClaustrophobia == hasClaustrophobia;
+
+            // Применяем оба делегата последовательно
+            var filteredByType = _pets.Where(typeFilter);
+            var filteredByBoth = filteredByType.Where(claustrophobiaFilter);
+
+            return filteredByBoth.Cast<Pet>().ToList();
+        }
+    }
+
+    public partial class Shelter<T> : IChangeable where T : Pet
+    {
+        /// Добавление животного в приют (реализация IChangeable)
+        public void AddPet(Pet pet)
+        {
+            if (pet is T typedPet)
+            {
+                // Проверка клаустрофобии
+                if (typedPet.HasClaustrophobia && !HasOpenArea)
+                {
+                    throw new InvalidOperationException(
+                        $"Животное {typedPet.Name} с клаустрофобией нельзя поместить в закрытый приют {Name}");
+                }
+
+                // Проверка вместимости
+                if (_pets.Count >= Capacity)
+                {
+                    throw new InvalidOperationException($"Приют {Name} переполнен!");
+                }
+
+                _pets.Add(typedPet);
+            }
+            else
+            {
+                throw new InvalidOperationException(
+                    $"Приют {Name} принимает только животных типа {typeof(T).Name}");
+            }
+        }
+
+        /// Удаление животного из приюта (реализация IChangeable)
+        public void RemovePet(Pet pet)
+        {
+            if (pet is T typedPet)
+            {
+                _pets.Remove(typedPet);
+            }
         }
     }
 }
